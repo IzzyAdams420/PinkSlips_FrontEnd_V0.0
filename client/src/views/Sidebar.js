1a379309468453e0aafdda9cc28f671d7a289814
Izzy <izzyadams420@icloud.com>
2021-07-08 21:36:31 -0700

    ._.

--- client/src/views/Sidebar.js
+++ client/src/views/Sidebar.js
@@ -1,13 +1,53 @@
 import React from 'react';
 
+import {Navbar} from 'react-bootstrap';
+
 
 const Sidebar = (props) => {
-    
-    return (
-        <div className="Sidebar">
-            <br />{"Hey!"}
-        </div>
-    );
+// verifies if routeName is the one active (in browser input)
+    const activeRoute = (routeName) => {
+        return props.location.pathname.indexOf(routeName) > -1 ? "active" : "";
+    };
+
+// creates the links that appear in the left menu / Sidebar
+  const createLinks = (routes) => {
+    return routes.map((prop, key) => {
+      return (
+        <NavItem key={key}>
+          <NavLink
+            to={prop.layout + prop.path}
+            tag={NavLinkRRD}
+            onClick={closeCollapse}
+            activeClassName="active"
+          >
+            <i className={prop.icon} />
+            {prop.name}
+          </NavLink>
+        </NavItem>
+      );
+    });
+  };
+
+  const { bgColor, routes, logo } = props;
+  let navbarBrandProps;
+  if (logo && logo.innerLink) {
+    navbarBrandProps = {
+      to: logo.innerLink,
+      tag: Link,
+    };
+  } else if (logo && logo.outterLink) {
+    navbarBrandProps = {
+      href: logo.outterLink,
+      target: "_blank",
+    };
+  }
+
+
+  return (
+    <div className="Sidebar">
+        <br />{"Hey!"}
+    </div>
+);
 }
 
 export default Sidebar;
