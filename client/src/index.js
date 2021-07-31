ffc562afea24042bbe9c52369505b37f877c5516
Izzy <izzyadams420@icloud.com>
2021-07-30 23:29:19 -0700

    Added Hash routing (as test for fleek)

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+import { HashRouter, Route, Switch, Redirect } from "react-router-dom";
 
 //import AdminLayout from "layouts/Admin.js";
 //import AuthLayout from "layouts/Auth.js";
 import App from './views/App';
 
 ReactDOM.render(
-
-    <BrowserRouter>
+  <HashRouter>
+   {/*<BrowserRouter> */}
+    
       <Switch>
         <Route path="/" render={(props) => <App />} />
         <Redirect from="/" to="/#/" />
       </Switch>
-    </BrowserRouter>,
+   {/* </BrowserRouter> */}
+   </HashRouter>,
 
     document.getElementById("root")
   
